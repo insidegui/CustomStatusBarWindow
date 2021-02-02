0d1e3f8faae4eb3b98c3dcd679c13b4b605c9d87
insidegui@gmail.com <insidegui@gmail.com>
2021-02-02 09:44:30 -0300

    Updates

--- CustomStatusBarWindow2/AppDelegate.swift
+++ CustomStatusBarWindow2/AppDelegate.swift
@@ -10,17 +10,54 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    private var statusItem: NSStatusItem!
     
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem = NSStatusBar.system.statusItem(withLength: 28)
+        statusItem.button?.title = "😊"
+        statusItem.button?.action = #selector(toggleUIVisible)
+    }
+    
+    private var windowController: StatusBarMenuWindowController?
+
+    @objc func toggleUIVisible(_ sender: Any?) {
+        if windowController == nil || windowController?.window?.isVisible == false {
+            showUI(sender: sender)
+        } else {
+            hideUI()
+        }
+    }
+    
+    @objc func hideUI() {
+        windowController?.close()
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    func showUI(sender: Any?) {
+        if windowController == nil {
+            windowController = StatusBarMenuWindowController(
+                statusItem: statusItem,
+                contentViewController: DummyContentViewController()
+            )
+        }
+        
+        windowController?.showWindow(sender)
     }
 
 
 }
 
+final class DummyContentViewController: NSViewController {
+    
+    convenience init() {
+        self.init(nibName: nil, bundle: nil)
+        
+        preferredContentSize = NSSize(width: 290, height: 300)
+    }
+    
+    override func loadView() {
+        view = NSView()
+        view.wantsLayer = true
+        view.layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+This sample project demonstrates a custom `NSWindowController` that can be attached to a `NSStatusItem` on macOS, such that it behaves similarly to `NSPopover`, but without the popover animations and pointy bit. The code was extracted from my app [AirBuddy](https://airbuddy.app).
+
+![screenshot](./screenshot.png)
+
+Creating the controller is easy, it needs a status item to be its "target", and a view controller for its contents:
+
+```swift
+windowController = StatusBarMenuWindowController(
+    statusItem: // your status item,
+    contentViewController: // any view controller
+)
+```
+
+**Important: the contentViewController must have its `preferredContentSize` set, the window will automatically resize itself based on that (including if it changes while visible).**
+
+To present the window attached to the status item:
+
+```swift
+windowController.showWindow(sender)
+```
+
+If the status item's underlying window can't be found, the window will be positioned centered on screen as a fallback.
+
+The implementation of `StatusBarMenuWindowController` also does a few things to make it behave more like system items like Control Center on macOS 11+, such as posting menu tracking notifications which cause the Menu Bar to stay expanded while its visible on top of a full screen application.
