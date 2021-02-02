36f6f5dfdc741beb5b88c78194396bb0e8f05487
Guilherme Rambo <insidegui@gmail.com>
2021-02-02 09:22:31 -0300

    Initial Commit

---
+++ CustomStatusBarWindow2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CustomStatusBarWindow2
+//
+//  Created by Gui Rambo on 02/02/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CustomStatusBarWindow2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CustomStatusBarWindow2
+//
+//  Created by Gui Rambo on 02/02/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
